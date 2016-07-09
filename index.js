4fbac5313146bf93ed68968972a9419625e8e45a
Kumar, Rajnish <rajnish.kumar@neustar.biz>
2016-07-09 01:05:03 -0700

    checking connection

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // index
 app.get('/', function (req, res) {
-	res.send('hello world i am a secret bot')
+	res.send('Lets start tracking your goal  ')
 })
 
 // for facebook verification
