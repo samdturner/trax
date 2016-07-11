e655d150563dcdd465667e04a247a78a4da6789b
Sam Turner <sam3turner@gmail.com>
2016-07-11 00:16:19 -0700

    this is going to work!!!!!!

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -10,12 +10,10 @@ exports.saveObj = function(req, res, Users){
 
   request(`${USER_PROFILE_URL}?access_token=${pageAccessToken}`, function(error, response, body) {
     if (!error && response.statusCode == 200) {
-      let user = Object.assign({}, body, { uid: req['user_id']});
-      console.log(JSON.stringify(user));
-      console.log(`The user is: ${user}`);
-      console.log(`The body is: ${body}`)
+      // let user = Object.assign({}, body, { uid: req['user_id']});
+      body.uid = req['user_id'];
       Users.create(
-        { user }
+        body
       ).then( function (users) {
         logger.info(__filename + " Test object created: " + users);
         return res.send(users);
