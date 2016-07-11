91fa2571ce676882fc69d349c01ae2911fc20419
Sam Turner <sam3turner@gmail.com>
2016-07-11 00:07:00 -0700

    bug fix - page access token needs to be defined

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,12 +1,14 @@
 const path   = require("path");
 const logger = require(path.join(__dirname, '../setup/init')).winston;
 const request = require('request');
+const constants = require(path.join(__dirname, "../setup/constants"));
+const pageAccessToken = constants.PAGE_ACCESS_TOKEN;
 
-const USER_DOMAIN = 'https://graph.facebook.com/v2.6/1356921494324269';
+const USER_PROFILE_URL = 'https://graph.facebook.com/v2.6/1356921494324269';
 
 exports.saveObj = function(req, res, Users){
 
-  request(`${USER_DOMAIN}?access_token=${pageAccessToken}`, function(error, response, body) {
+  request(`${USER_PROFILE_URL}?access_token=${pageAccessToken}`, function(error, response, body) {
     if (!error && response.statusCode == 200) {
       let user = Object.assign({}, body, { uid: req['user_id']});
       console.log(user);
