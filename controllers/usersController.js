0258253aeb8e28040349029117bc6e5bbe9a1762
Sam Turner <sam3turner@gmail.com>
2016-07-11 00:14:00 -0700

    console log for debugging

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -11,7 +11,9 @@ exports.saveObj = function(req, res, Users){
   request(`${USER_PROFILE_URL}?access_token=${pageAccessToken}`, function(error, response, body) {
     if (!error && response.statusCode == 200) {
       let user = Object.assign({}, body, { uid: req['user_id']});
-      console.log(user);
+      console.log(JSON.stringify(user));
+      console.log(`The user is: ${user}`);
+      console.log(`The body is: ${body}`)
       Users.create(
         { user }
       ).then( function (users) {
