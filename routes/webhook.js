86a47e604e10cacfd0950e921309ea5f266fe108
Sam Turner <sam3turner@gmail.com>
2016-07-13 23:53:55 -0700

    uncomment the test controller

--- routes/webhook.js
+++ routes/webhook.js
@@ -63,8 +63,8 @@ module.exports = function(app){
     console.log('In POST request');
 		let messaging_events = req.body.entry[0].messaging
 
-		//Test DB Connection
-		//testController.saveObj(req, res, app.models.Test)
+		// Test DB Connection
+		testController.saveObj(req, res, app.models.Test)
 
 		for (let i = 0; i < messaging_events.length; i++) {
 			let event = req.body.entry[0].messaging[i]
--- setup/constants.js
+++ setup/constants.js
@@ -15,14 +15,20 @@ module.exports = {
     HTTPS_PORT  : 443,
     TABLES: {
         GOALS: "goals",
-        USERS: "users" 
+        USERS: "users"
     },
     HTTPS_KEY : "/etc/letsencrypt/live/goaltracker.us/privkey.pem",
     HTTPS_CERT: "/etc/letsencrypt/live/goaltracker.us/fullchain.pem",
     DB_FORCE_CREATE : "false",  //this informs if we need to clean up all table's data unpon initialization
-	PAGE_ACCESS_TOKEN: "EAAYv0vhTxk4BAC3LcTowQsjXtCWqWytg3rZCAa3e5b0oOTHNByZByPiRkg5UAxBDCml0aUi4aIuq62wgTw0L4I43tIf5f32x2rvjqfZB0D53ZB3OXVJolmu3nnTDn4yBf4ho5YG9sYHZBEE1UX2fEBL9tekoWe3gD2T4pdLxfFgZDZD",
-	VERIFY_TOKEN: "this_is_my_trax_password",
-	REDIS_HOST: "facebook-trax.fexhvp.0001.usw2.cache.amazonaws.com",
-	REDIS_PORT: 6379
+		REDIS_HOST: "facebook-trax.fexhvp.0001.usw2.cache.amazonaws.com",
+		REDIS_PORT: 6379,
+		PAGE_ACCESS_TOKEN: "EAAYv0vhTxk4BAC3LcTowQsjXtCWqWytg3rZCAa3e5b0oOTHNByZByPiRkg5UAxBDCml0aUi4aIuq62wgTw0L4I43tIf5f32x2rvjqfZB0D53ZB3OXVJolmu3nnTDn4yBf4ho5YG9sYHZBEE1UX2fEBL9tekoWe3gD2T4pdLxfFgZDZD",
+		VERIFY_TOKEN: "this_is_my_trax_password",
+		PAYLOAD_GOAL_GYM: "GOAL_GYM",
+		PAYLOAD_GOAL_WAKE_UP: "GOAL_WAKE_UP",
+		PAYLOAD_FREQUENCY_WEEKDAYS: "FREQUENCY_WEEKDAYS",
+		PAYLOAD_FREQUENCY_WEEKENDS: "FREQUENCY_WEEKENDS",
+		PAYLOAD_FREQUENCY_EVERY_DAY: "FREQUENCY_EVERY_DAY",
+		PAYLOAD_PROGRESS_COMPLETE: "PROGRESS_COMPLETE",
+		PAYLOAD_PROGRESS_INCOMPLETE: "PROGRESS_INCOMPLETE"
 }
-
